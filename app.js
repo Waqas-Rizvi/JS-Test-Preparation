a308cb3fdc51ee9a00b5075ce4b259067f15a62a
Muhammad Waqas Rizvi <waqashafiz90@gmail.com>
2021-01-20 23:23:44 +0500

    40Chapters' practice had completed

--- app.js
+++ app.js
@@ -319,3 +319,121 @@ alert(pera)*/
 // var f = +prompt("w")
 // alert(f + 2)
 
+
+//"Preparation of Test#04"
+
+// var text = "My name is Waqas"
+// var lower = text.toLowerCase()
+// var upper = text.toUpperCase()
+// var index = text.indexOf("name")
+// var slice = text.slice((index + 4), 16)
+// var first = text.slice(index, 7)
+// var cap = first.slice(0,1).toUpperCase()
+// var small = first.slice(1).toLowerCase()
+// var comp = text.slice(0, index) + cap + small + slice
+// alert(comp)
+
+// var text = "My name is Waqas Altaf"
+// var index = text.indexOf("Waqas")
+// alert(text)
+// if(index !== -1){
+//     text = text.slice(0, index) + "Hassan" + text.slice((index + 5))
+// } else {}
+
+// var text = "My name is Waqas Altaf"
+// var replace = text.replace("Waqas", "Hassan")
+// alert(replace)
+
+// var text = "My name is Waqas Altaf"
+// var char = text.charAt(3)
+// alert(char)
+
+// var text = "To be or not to be"
+// var index = text.lastIndexOf("be")
+// alert(index)
+
+// var text = "To be or not to be"
+// var replace = text.replace(/be/g, "a")
+// alert(replace)
+
+// var num = +prompt()
+// var round = Math.round(num)
+// var ceil = Math.ceil(num)
+// var floor = Math.floor(num)
+// document.write(round + " " + ceil + " " + floor) 
+
+// var random = Math.random()
+// var num = random * 10
+// var ceil = Math.ceil(num)
+// alert(ceil)
+
+// var a = "15.5"
+// var d = parseFloat(a)
+// var b = 15
+// var c = d + b
+// alert(c)
+
+// var a = "15"
+// var d = parseInt(a)
+// var b = 15
+// var c = d + b
+// alert(c)
+
+// var a = "15.3"
+// var d = Number(a)
+// var b = 15
+// var c = d + b
+// alert(c)
+
+// var a = 15
+// var b = 14
+// var d = b.toString() 
+// var c = a + d
+// alert(c)
+
+// var a = 15
+// var b = 14
+// var c = a / b
+// var d = c.toFixed(2) 
+// alert(d)
+
+// var a = new Date()
+// var days = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"]
+// var b = a.getDay()
+// alert(days[b])
+
+// var a = new Date()
+// a.setFullYear(1998)
+// a.setMonth(8)
+// a.setDate(19)
+// var days = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"]
+// var b = a.getDay()
+// alert(days[b])
+
+// function add(a, b){
+//     alert(a + b)
+// }
+// add(8, 3)
+
+// var day = "sun"
+// switch (day) {
+//     case "sun":
+//         alert("waqas")
+//         break
+//     default:
+//         alert("hamza")
+// }
+
+// var i = 1
+// while (i < 5) {
+//     alert(i)
+//     i++
+// }
+
+// var i = 1
+// do {
+//     alert(i)
+//     i++
+//     alert(i)
+// } while (i < 5)
+
